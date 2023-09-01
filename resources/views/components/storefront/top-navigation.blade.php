08432a3a3a7a01b06a891e8e38296cf05bbe04fc
Captain Buckman <ZeroGodForce@users.noreply.github.com>
2023-09-01 15:42:50 +0100

    [USER] [WIP] Added routes to signup/login links but conditional rendering not functioning as desired

--- resources/views/components/storefront/top-navigation.blade.php
+++ resources/views/components/storefront/top-navigation.blade.php
@@ -253,12 +253,19 @@
                 </div>
 
                 <div class="space-y-6 border-t border-gray-200 px-4 py-6">
-                    <div class="flow-root">
-                        <a href="#" class="-m-2 block p-2 font-medium text-gray-900">Sign in</a>
-                    </div>
-                    <div class="flow-root">
-                        <a href="#" class="-m-2 block p-2 font-medium text-gray-900">Create account</a>
-                    </div>
+                    {{-- TODO: FIX RENDERING ON THIS --}}
+                    @auth
+                        <div class="flow-root">
+                            <a href="{{ url('/dashboard') }}" class="-m-2 block p-2 font-medium text-gray-900">Account</a>
+                        </div>
+                    @else
+                        <div class="flow-root">
+                            <a href="{{ route('login') }}" class="-m-2 block p-2 font-medium text-gray-900">Sign in</a>
+                        </div>
+                        <div class="flow-root">
+                            <a href="{{ route('register') }}" class="-m-2 block p-2 font-medium text-gray-900">Create account</a>
+                        </div>
+                    @endauth
                 </div>
 
                 <div class="border-t border-gray-200 px-4 py-6">
