e3fa556b1267e668dfcc4f40d03143c8d4206ea9
Captain Buckman <ZeroGodForce@users.noreply.github.com>
2023-09-04 12:51:00 +0100

    [FOOTER] Simplified

--- resources/views/components/storefront/footer.blade.php
+++ resources/views/components/storefront/footer.blade.php
@@ -1,102 +1,15 @@
-<footer aria-labelledby="footer-heading" class="bg-white">
-    <h2 id="footer-heading" class="sr-only">Footer</h2>
-    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div class="py-20 xl:grid xl:grid-cols-3 xl:gap-8">
-            <div class="grid grid-cols-2 gap-8 xl:col-span-2">
-                <div class="space-y-16 md:grid md:grid-cols-2 md:gap-8 md:space-y-0">
-                    <div>
-                        <h3 class="text-sm font-medium text-gray-900">Shop</h3>
-                        <ul role="list" class="mt-6 space-y-6">
-                            <li class="text-sm">
-                                <a href="#" class="text-gray-500 hover:text-gray-600">Bags</a>
-                            </li>
-                            <li class="text-sm">
-                                <a href="#" class="text-gray-500 hover:text-gray-600">Tees</a>
-                            </li>
-                            <li class="text-sm">
-                                <a href="#" class="text-gray-500 hover:text-gray-600">Objects</a>
-                            </li>
-                            <li class="text-sm">
-                                <a href="#" class="text-gray-500 hover:text-gray-600">Home Goods</a>
-                            </li>
-                            <li class="text-sm">
-                                <a href="#" class="text-gray-500 hover:text-gray-600">Accessories</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h3 class="text-sm font-medium text-gray-900">Company</h3>
-                        <ul role="list" class="mt-6 space-y-6">
-                            <li class="text-sm">
-                                <a href="#" class="text-gray-500 hover:text-gray-600">Who we are</a>
-                            </li>
-                            <li class="text-sm">
-                                <a href="#" class="text-gray-500 hover:text-gray-600">Sustainability</a>
-                            </li>
-                            <li class="text-sm">
-                                <a href="#" class="text-gray-500 hover:text-gray-600">Press</a>
-                            </li>
-                            <li class="text-sm">
-                                <a href="#" class="text-gray-500 hover:text-gray-600">Careers</a>
-                            </li>
-                            <li class="text-sm">
-                                <a href="#" class="text-gray-500 hover:text-gray-600">Terms &amp; Conditions</a>
-                            </li>
-                            <li class="text-sm">
-                                <a href="#" class="text-gray-500 hover:text-gray-600">Privacy</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="space-y-16 md:grid md:grid-cols-2 md:gap-8 md:space-y-0">
-                    <div>
-                        <h3 class="text-sm font-medium text-gray-900">Account</h3>
-                        <ul role="list" class="mt-6 space-y-6">
-                            <li class="text-sm">
-                                <a href="#" class="text-gray-500 hover:text-gray-600">Manage Account</a>
-                            </li>
-                            <li class="text-sm">
-                                <a href="#" class="text-gray-500 hover:text-gray-600">Returns &amp; Exchanges</a>
-                            </li>
-                            <li class="text-sm">
-                                <a href="#" class="text-gray-500 hover:text-gray-600">Redeem a Gift Card</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div>
-                        <h3 class="text-sm font-medium text-gray-900">Connect</h3>
-                        <ul role="list" class="mt-6 space-y-6">
-                            <li class="text-sm">
-                                <a href="#" class="text-gray-500 hover:text-gray-600">Contact Us</a>
-                            </li>
-                            <li class="text-sm">
-                                <a href="#" class="text-gray-500 hover:text-gray-600">Twitter</a>
-                            </li>
-                            <li class="text-sm">
-                                <a href="#" class="text-gray-500 hover:text-gray-600">Instagram</a>
-                            </li>
-                            <li class="text-sm">
-                                <a href="#" class="text-gray-500 hover:text-gray-600">Pinterest</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="mt-16 md:mt-16 xl:mt-0">
-                <h3 class="text-sm font-medium text-gray-900">Sign up for our newsletter</h3>
-                <p class="mt-6 text-sm text-gray-500">The latest deals and savings, sent to your inbox weekly.</p>
-                <form class="mt-2 flex sm:max-w-md">
-                    <label for="email-address" class="sr-only">Email address</label>
-                    <input id="email-address" type="text" autocomplete="email" required class="w-full min-w-0 appearance-none rounded-md border border-gray-300 bg-white px-4 py-2 text-base text-indigo-500 placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500">
-                    <div class="ml-4 flex-shrink-0">
-                        <button type="submit" class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Sign up</button>
-                    </div>
-                </form>
-            </div>
+<footer class="bg-white">
+    <div class="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
+        <div class="flex justify-center space-x-6 md:order-2">
+            <a href="https://github.com/ZeroGodForce/drop-kicks" class="text-gray-400 hover:text-gray-500">
+                <span class="sr-only">GitHub</span>
+                <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clip-rule="evenodd" />
+                </svg>
+            </a>
         </div>
-
-        <div class="border-t border-gray-200 py-10">
-            <p class="text-sm text-gray-500">Copyright &copy; 2021 Your Company, Inc.</p>
+        <div class="mt-8 md:order-1 md:mt-0">
+            <p class="text-center text-xs leading-5 text-gray-500">&copy; Drop Kicks Inc, an experimental shopping site.</p>
         </div>
     </div>
 </footer>
